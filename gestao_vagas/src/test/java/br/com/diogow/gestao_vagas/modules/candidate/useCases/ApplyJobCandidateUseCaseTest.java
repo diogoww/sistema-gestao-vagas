b3bf6b3c5f1ae3f880cd016f89222238beed5c20
Diogo Varaschin <diogovaraschin@outlook.com>
2025-08-18 14:15:20 -0300

    test: validadte job does not exist

--- gestao_vagas/src/test/java/br/com/diogow/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
+++ gestao_vagas/src/test/java/br/com/diogow/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -1,6 +1,8 @@
 package br.com.diogow.gestao_vagas.modules.candidate.useCases;
 
+import br.com.diogow.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.diogow.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.diogow.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.diogow.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.diogow.gestao_vagas.modules.company.repositories.JobRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +12,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ApplyJobCandidateUseCaseTest {
@@ -31,6 +38,22 @@ public class ApplyJobCandidateUseCaseTest {
             applyJobCandidateUseCase.execute(null, null);
         } catch (Exception e){
             assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+
+    @Test
+    public void should_not_be_able_to_apply_job_with_not_found(){
+        var idCandidate = UUID.randomUUID();
+
+        var candidate = new CandidateEntity();
+        candidate.setId(idCandidate);
+
+        when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+        
+        try{
+            applyJobCandidateUseCase.execute(idCandidate, null);
+        } catch (Exception e){
+            assertThat(e).isInstanceOf(JobNotFoundException.class);
         }
     }
 }
