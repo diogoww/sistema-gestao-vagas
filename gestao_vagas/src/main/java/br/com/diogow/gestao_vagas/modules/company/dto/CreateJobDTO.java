e7c8a7bf6832809361ddc3047f5891d5af66d8ed
Diogo Varaschin <diogovaraschin@outlook.com>
2025-08-19 14:40:18 -0300

    feat: JobController - create the integration test

--- gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,9 +1,15 @@
 package br.com.diogow.gestao_vagas.modules.company.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateJobDTO {
     @Schema(example = "Vaga para pessoal desenvolvedora JR", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
